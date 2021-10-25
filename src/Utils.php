4be515ff7b14177a7f63f0ce6a4a285bfc4e7ed6
Grigory Tydykov <tydykov2022@mail.ru>
2021-10-25 08:47:36 +0600

    add gendiff

---
+++ src/Utils.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Differ\Utils;
+
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $file1 = file_get_contents($pathToFile1);
+    $file2 = file_get_contents($pathToFile2);
+
+    $data1 = json_decode($file1, true);
+    $data2 = json_decode($file2, true);
+
+    $dataMerge = array_merge($data1, $data2);
+
+    ksort($dataMerge);
+
+    $result = [];
+
+    foreach ($dataMerge as $key => $value) {
+
+        $normalizeValue = json_encode($value);
+
+        $template = "{$key}: {$normalizeValue}";
+
+        if (!array_key_exists($key, $data1)) {
+            $result[] = "+ {$template}";
+        } elseif (!array_key_exists($key, $data2)) {
+            $result[] = "- {$template}";
+        } elseif ($value !== $data1[$key]) {
+            $result[] = "- {$key}: {$data1[$key]}";
+            $result[] = "+ {$template}";
+        } else {
+            $result[] = "  {$template}";
+        }
+    }
+
+    return implode("\n", $result) . "\n";
+}
