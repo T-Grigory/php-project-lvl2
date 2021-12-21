a3a2994d4b3f068d05009ff3c35c782e96d767c1
Grigory Tydykov <tydykov2022@mail.ru>
2021-12-21 15:14:57 +0600

    fixed test

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -17,25 +17,24 @@ class DifferTest extends TestCase
      * @dataProvider diffProvider
      */
 
-    public function testDiff($file1, $file2, $format, $expected): void
+    public function testDiff($file1, $file2, $format, $expectedFileName): void
     {
         $actual = genDiff($this->getFilePath($file1), $this->getFilePath($file2), $format);
+        $content = file_get_contents($this->getFilePath($expectedFileName));
+        $expected = $expectedFileName === 'json.txt' ? str_replace(["-", "\n"], "", $content) : $content;
+
         $this->assertEquals($expected, $actual);
     }
 
     public function diffProvider(): array
     {
-        $expectedNested = file_get_contents($this->getFilePath('nested.txt'));
-        $expectedPlain = file_get_contents($this->getFilePath('plain.txt'));
-        $expectedJson = str_replace(["-", "\n"], "", file_get_contents($this->getFilePath('json.txt')));
-
         return [
-            ['file1.json', 'file2.json', 'stylish', $expectedNested],
-            ['file1.yml', 'file2.yml', 'stylish', $expectedNested],
-            ['file1.json', 'file2.json', 'plain', $expectedPlain],
-            ['file1.yml', 'file2.yml', 'plain', $expectedPlain],
-            ['file1.json', 'file2.json', 'json', $expectedJson],
-            ['file1.yml', 'file2.yml', 'json', $expectedJson]
+            ['file1.json', 'file2.json', 'stylish', 'nested.txt'],
+            ['file1.yml', 'file2.yml', 'stylish', 'nested.txt'],
+            ['file1.json', 'file2.json', 'plain', 'plain.txt'],
+            ['file1.yml', 'file2.yml', 'plain', 'plain.txt'],
+            ['file1.json', 'file2.json', 'json', 'json.txt'],
+            ['file1.yml', 'file2.yml', 'json', 'json.txt']
         ];
     }
 }
