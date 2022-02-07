8e9150420f92d832d3a3883ed38400a1421bae19
Grigory Tydykov <tydykov2022@mail.ru>
2022-02-07 09:54:40 +0700

    fix parser

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,10 +6,8 @@ use Symfony\Component\Yaml\Yaml;
 
 function getDataParsing(string $data, string $extension): object
 {
-    $extension = $extension === 'yaml' ? 'yml' : $extension;
-
     return match ($extension) {
-        'yml' => Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP),
+        'yml', 'yaml' => Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP),
         'json' => json_decode($data),
         default => throw new \Exception("uknown extension: '{$extension}'!"),
     };
